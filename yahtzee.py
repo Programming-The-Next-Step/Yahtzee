ba3b01cf997e649c36dc618ce25f17695c806e78
DanaRavestein <dana.ravestein94@gmail.com>
2020-05-08 16:06:48 +0200

    Roll dice and create scorecard

--- yahtzee.py
+++ yahtzee.py
@@ -3,26 +3,219 @@ from random import randint
 import numpy as np
 import pygame as pg
 
-'''  Check classes tomorrow for DiceSet?
+ 
+ #define the Dice class
 class Dice:
-    def __init__(self, value):
-        self.value = value        
- '''      
+    def __init__(self):
+        self.current_throw = [randint(1, 6) for i in range(0, 5)]       
+    def roll(self, throw):
+        for j in [int(i) - 1 for i in throw]:
+            self.current_throw[j] = randint(1, 6) 
+    def manual_set(self, a):
+        self.current_throw = a
 
-
+# present throw, ask input player, roll again
 num_throws = 3
-num_dice = 5
-dice = np.zeros([num_dice])
 for i in range(0, num_throws):
     if i == 0:
-        throw_again_idx = range(0, len(dice))
+        player1 = Dice()
     else:
-        throw_again_idx = np.array([int(i) - 1 for i in input().split()])
-  
-    for j in throw_again_idx:
-            dice[j] = randint(1, 6)   
-            
-    print('Do you want to keep these dice, press k?: ', dice)  
-    
-    if input() == 'k':
+        keep_dice = np.array(input('Press y if you want to keep all the dice, press any key if you want to roll the dice again '))
+        if keep_dice == 'y':
+            print('Your throw is: ', player1.current_throw)
             break
+        throw_again_idx = np.array(input('Which di(c)e would you like to roll again? use a space between the numbers (e.g. 1 2 5) ').split())
+
+        player1.roll(throw_again_idx) 
+        
+        
+    print('You rolled: ', player1.current_throw)
+
+
+class Aces:
+    def __init__(self):
+        self.name = 'Aces'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts= np.unique(current_throw, return_counts = True)
+        if any(unique == 1):
+            self.possible = True
+    def score(self, current_throw):
+        self.points = np.sum([x for x in current_throw if x == 1])
+        
+class Twos:
+    def __init__(self):
+        self.name = 'Twos'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts= np.unique(current_throw, return_counts = True)
+        if any(unique == 2):
+            self.possible = True
+    def score(self, current_throw):
+        self.points = np.sum([x for x in current_throw if x == 2])
+
+class Threes:
+    def __init__(self):
+        self.name = 'Threes'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts= np.unique(current_throw, return_counts = True)
+        if any(unique == 3):
+            self.possible = True
+    def score(self, current_throw):
+        self.points = np.sum([x for x in current_throw if x == 3])
+        
+class Fours:
+    def __init__(self):
+        self.name = 'Fours'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts= np.unique(current_throw, return_counts = True)
+        if any(unique == 4):
+            self.possible = True
+    def score(self, current_throw):
+        self.points = np.sum([x for x in current_throw if x == 4])
+        
+class Fives:
+    def __init__(self):
+        self.name = 'Fives'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts= np.unique(current_throw, return_counts = True)
+        if any(unique == 5):
+            self.possible = True
+    def score(self, current_throw):
+        self.points = np.sum([x for x in current_throw if x == 5])
+        
+class Sixes:
+    def __init__(self):
+        self.name = 'Sixes'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts= np.unique(current_throw, return_counts = True)
+        if any(unique == 6):
+            self.possible = True
+    def score(self, current_throw):
+        self.points = np.sum([x for x in current_throw if x == 6])
+        
+class Three_of_kind:
+    def __init__(self):
+        self.name = 'Three of a kind'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts= np.unique(current_throw, return_counts = True)
+        if any(counts >= 3):
+            self.possible = True
+    def score(self, current_throw):
+        self.points = np.sum(current_throw)
+            
+class Four_of_kind:
+    def __init__(self):
+        self.name = 'Four of a kind'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts = np.unique(current_throw, return_counts = True)
+        if any(counts >= 4):
+            self.possible = True
+    def score(self, current_throw):
+        self.points = np.sum(current_throw)      
+
+class Full_house:
+    def __init__(self):
+        self.name = 'Full House'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts = np.unique(current_throw, return_counts = True)
+        if any(counts == 3) and any(counts == 2) and len(unique == 2):
+            self.possible = True
+    def score(self, current_throw):
+        self.points = 25  
+
+class Small_straight:
+    def __init__(self):
+        self.name = 'Small Straight'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts = np.unique(current_throw, return_counts = True)
+        if len(unique) >= 4:
+            self.possible = True
+    def score(self, current_throw):
+        self.points = 30  
+    
+class Large_straight:
+    def __init__(self):
+        self.name = 'Large Straight'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts = np.unique(current_throw, return_counts = True)
+        if len(unique) == 5:
+            self.possible = True
+    def score(self, current_throw):
+        self.points = 40 
+
+class Yahtzee:
+    def __init__(self):
+        self.name = 'Yahtzee'
+        self.possible = False
+        self.points = 0
+    def is_possible(self, current_throw):
+        unique, counts = np.unique(current_throw, return_counts = True)
+        if any(counts == 5):
+            self.possible = True
+    def score(self, current_throw):
+        self.points = 50
+
+class Chance:
+    def __init__(self):
+        self.name = 'Chance'
+        self.possible = False
+        self.points = 0
+        self.filled = False
+    def is_possible(self, current_throw):
+        self.possible = True
+    def score(self, current_throw):
+        self.points = np.sum(current_throw)
+        
+     
+
+aces = Aces()
+twos = Twos()
+threes = Threes()
+fours = Fours()
+fives = Fives()
+sixes = Sixes()
+three_of_kind = Three_of_kind()
+four_of_kind = Four_of_kind()
+full_house = Full_house()
+small_straight = Small_straight()
+large_straight = Large_straight()
+chance = Chance()
+yahtzee = Yahtzee()
+
+scorecard = [aces, twos, threes, fours, fives, sixes, three_of_kind, four_of_kind, full_house, small_straight, large_straight, yahtzee, chance]
+
+for i, category in enumerate(scorecard):
+    category.is_possible(player1.current_throw)
+    category.score(player1.current_throw)
+
+    print(category.name, ' is possible: ', category.possible, 'points: ', category.points )
+
+
+
+
+
+    
+    
+    
+    
